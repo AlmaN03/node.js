a82a46bb0ab994b8080b98f2c656b86096dc1eef
AlmaN03 <shinshin04@mail.ru>
2022-03-10 20:17:55 +0300

    ввод

---
+++ array.js
@@ -0,0 +1,12 @@
+
+
+var array_counter = function (array) {
+    return "в масиве находится " + array.length+" элементов"
+}
+
+
+//console.log(array_counter([1, 7 ,99, 8 ,45,8]));
+
+//экспорт модуля 
+
+module.exports = array_counter;
--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+
+
+/*
+function test() {
+    console.log("Hello");
+}
+
+test();
+
+function call(func) {
+    func();
+}
+
+
+
+var printSomething = function () {
+    console.log("просто текст");
+};
+
+printSomething();
+
+
+
+call(printSomething);
+*/
+
+//подключенеи модуля 
+ // require ('./array');
+
+
+var counter = require ('./array');
+
+console.log(counter([1, 7 ,99, 8 ,45,8,6]));
+
+
+
+
